4469b3c44b2182db076fb4a8b3d3dbbe73c80f32
Ritik Vishwakarma <150435625+Ritikvis@users.noreply.github.com>
2024-02-18 15:38:09 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/suduko_solver/SUDUKO_SOLVER.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.suduko_solver;
+
+/**
+ *
+ * @author Siddharth
+ */
+public class SUDUKO_SOLVER {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
